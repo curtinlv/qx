3f82117b6236777e3a79129dbe167c95ea0cee4f
curtinlv <78124261+curtinlv@users.noreply.github.com>
2023-06-15 15:22:53 +0800

    Update pkc-mt.js
    1

--- scripts/pkc-mt.js
+++ scripts/pkc-mt.js
@@ -176,8 +176,8 @@ async function pkc_mtqj(timeout = 0) {
                         pkc_flag = true;
                     }else if ($.signget.code === 1 && $.signget.subcode === 2){
                         console.log(`[${new Date().toLocaleString('en-US', { timeZone: 'Asia/Shanghai', hour12: false, month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 }).replace(',', '').replace(/\//g, '-')}]【成功抢券】：${$.signget.msg}\n`);
-                        $.message += `【成功抢券】：${$.signget.msg}\n`;
-                        pkc_flag = true;
+                        $.message += `【继续尝试】：${$.signget.msg}\n`;
+                       // pkc_flag = true;
                     }
 //                     else if (($.signget.code === 1) && ($.signget.subcode === 1 || $.signget.subcode === 11)){
 //                         console.log(`【抢券失败】：${data}\n`);
