954c879e756f1abeb4cb20977856ddfebae2eba5
curtinlv <curtinlv@126.com>
2025-05-31 00:12:09 +0800

    修复美团脚本

--- scripts/pkc-mt0807.js
+++ scripts/pkc-mt0807.js
@@ -435,13 +435,13 @@ async function pkc_mtqj_xtll(timeout = 0) {
                 try {
                     if (logs) $.log(`开始抢券🚩: ${data}`);
                     $.signget = JSON.parse(data);
-                    if ($.signget.data.coupon.status === 3 && $.signget.data.coupon.toastMsg && $.signget.data.coupon.toastMsg.indexOf("成功") >= 0){
+                    if ($.signget['data']['coupon']['status'] === 3 && $.signget['data']['coupon']['toastMsg'] && $.signget['data']['coupon'][toastMsg].indexOf("成功") >= 0){
                         console.log(`[${new Date().toLocaleString('en-US', { timeZone: 'Asia/Shanghai', hour12: false, month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 }).replace(',', '').replace(/\//g, '-')}]【成功抢券】：${$.signget.data.coupon.toastMsg}\n`);
                         $.message += `【成功抢券】：${JSON.stringify($.signget)}\n`;
                         pkc_flag = true;
                     }else{
-                        console.log(`【继续尝试】：${JSON.stringify($.signget.data.coupon.toastMsg)}\n`);
-                        $.message += `【继续尝试】：${JSON.stringify($.signget.data.coupon.toastMsg)}\n`;
+                        console.log(`【继续尝试】：${JSON.stringify($.signget['data']['coupon']['toastMsg'])}\n`);
+                        $.message += `【继续尝试】：${JSON.stringify($.signget['data']['coupon']['toastMsg'])}\n`;
                     }
                 } catch (e) {
                     $.logErr(e, resp);
